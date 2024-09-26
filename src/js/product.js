67d0059d21e8e3b8146b351009c2ac386a2d2b61
O'Moncada <oscar.moncada702@gmail.com>
2024-09-25 22:39:02 -0300

    cambios en product.js

--- src/js/product.js
+++ src/js/product.js
@@ -7,3 +7,25 @@ const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
+
+// función findProductById
+console.log(dataSource.findProductById(productId));
+
+function addProductToCart(product) {
+    let cartItems = getLocalStorage("so-cart") || []; // get current cart or initialize an empty array
+    cartItems.push(product); // add new product to cart
+    setLocalStorage("so-cart", cartItems); // save updated cart
+
+    //console.log("Product added to cart:", product);
+    //console.log("Current Cart (localStorage):", getLocalStorage("so-cart"));
+}
+// add to cart button event handler
+async function addToCartHandler(e) {
+    const product = await dataSource.findProductById(e.target.dataset.id);
+    addProductToCart(product);
+}
+
+// add listener to Add to Cart button
+document
+    .getElementById("addToCart")
+    .addEventListener("click", addToCartHandler);
