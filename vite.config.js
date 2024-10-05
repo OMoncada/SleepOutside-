fd52a6cdf622daea60a1bbeab986297d9c33fe73
dommmy200 <constantx003@gmail.com>
2024-10-05 04:41:31 +0100

    Corrected LF to CRLF run: git config core.autocrlf true

--- vite.config.js
+++ vite.config.js
@@ -11,11 +11,7 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
-<<<<<<< HEAD
-        listing: resolve(__dirname, "src/product-listing/index.html"),
-=======
         Productlist: resolve(__dirname, "src/product-listing/index.html"),
->>>>>>> 189d5c9d3a73b4ecb59110fdfd3be5aac3ae0eea
       },
     },
   },
