a303bba20ae88c86934991c21f7b0ef65cbb1981
dommmy200 <constantx003@gmail.com>
2024-10-07 04:13:24 +0100

    mmm

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -39,3 +39,15 @@ searchInput.addEventListener("keyup", (event) => {
   const filteredList = productListing.filterList(query);
   productListing.renderList(filteredList);
 });
+
+// import { loadHeaderFooter, getParam } from "./utils.mjs";
+// import ProductData from "./ProductData.mjs";
+// import ProductList from "./ProductList.mjs";
+
+// loadHeaderFooter();
+// const category = getParam("category");
+// const dataSource = new ProductData();
+// const element = document.querySelector(".product-list");
+// const listing = new ProductList(category, dataSource, element);
+
+// listing.init();
